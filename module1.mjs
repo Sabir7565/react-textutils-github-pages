3cc66a1459ff3b9a8ec7f9492c0a9ef8770fe4c1
Sabir7565 <mohdsabir7565@gmail.com>
2022-01-09 02:14:22 +0530

    Updates

--- module1.mjs
+++
@@ -1,5 +0,0 @@
-import ui, {b, c, d1} from './module2.mjs'
-console.log(ui)
-console.log(b)
-console.log(c)
-console.log(d1)
--- module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = "harry";
-const b = "Rohan";
-const c = "Akash";
-const d = "Priyanka";
-
-export default a;
-export {b};
-export {c};
-export {d};
